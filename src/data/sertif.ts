be2112bef05a8d79d169c94e94edff104f6393e2
M Raihan Al Ayyubi <raihan2off@gmail.com>
2025-12-05 01:37:47 +0700

    update

--- src/data/sertif.ts
+++ src/data/sertif.ts
@@ -13,22 +13,6 @@ export const sertifData = {
     "M RAIHAN AL AYYUBI_Toefl.pdf",
   ],
 
-  coursera: [
-    "Administrasi Sistem dan Layanan Infrastruktur TI.pdf",
-    "Dasar-Dasar Dukungan Teknis.pdf",
-    "Keamanan IT Pertahanan terhadap Kejahatan Digital.pdf",
-    "Seluk Beluk Jaringan Komputer.pdf",
-    "Sistem Operasi dan Anda Menjadi Pengguna yang Berdaya.pdf",
-  ],
-
-  dicoding: [
-    "Belajar Dasar Manajemen Proyek.pdf",
-    "Belajar Prinsip Pemrograman SOLID.pdf",
-    "Memulai Pemrograman Dengan C.pdf",
-    "Memulai Pemrograman dengan Haskell.pdf",
-    "Memulai Pemrograman Dengan Java.pdf",
-  ],
-
   MyskillUIUXDesign: [
     "COLLABORATION IN FIGMA.pdf",
     "DESIGN DOCUMENTATION.pdf",
@@ -54,32 +38,48 @@ export const sertifData = {
     "WIREFRAMING FOR WEBSITE.pdf",
   ],
 
-  MyskillUXDesign: [
-  "AFFINITY MAPPING & INFORMATION ARCHITECTURE.pdf",
-  "CREATING LOW-FIDELITY DESIGN.pdf",
-  "CREATING USER FLOW & USER JOURNEY MAP.pdf",
-  "DEFINING PRODUCT BEFORE DESIGN.pdf",
-  "FIGMA INTRODUCTION.pdf",
-  "PROBLEM IDENTIFICATION & PRIORITIZATION.pdf",
-  "USER EXPERIENCE FACTORS.pdf",
-  "UX DESIGN FUNDAMENTAL.pdf",
-  "UX DESIGN PROCESS.pdf",
-  "UX IDEATION PROCESS.pdf",
+   MyskillUXDesign: [
+   "AFFINITY MAPPING & INFORMATION ARCHITECTURE.pdf",
+   "CREATING LOW-FIDELITY DESIGN.pdf",
+   "CREATING USER FLOW & USER JOURNEY MAP.pdf",
+   "DEFINING PRODUCT BEFORE DESIGN.pdf",
+   "FIGMA INTRODUCTION.pdf",
+   "PROBLEM IDENTIFICATION & PRIORITIZATION.pdf",
+   "USER EXPERIENCE FACTORS.pdf",
+   "UX DESIGN FUNDAMENTAL.pdf",
+   "UX DESIGN PROCESS.pdf",
+   "UX IDEATION PROCESS.pdf",
 ],
 
-  MongoDB: [
-  "Connecting to a MongoDB Database.pdf",
-  "Getting Started with MongoDB Atlas.pdf",
-  "Introduction to MongoDB.pdf",
-  "MongoDB Aggregation.pdf",
-  "MongoDB and the Document Model.pdf",
-  "MongoDB Atlas Search.pdf",
-  "MongoDB CRUD Operations Insert and Find Documents.pdf",
-  "MongoDB CRUD Operations Modifying Query Results.pdf",
-  "MongoDB CRUD Operations Replace and Delete Documents.pdf",
-  "MongoDB Data Modeling Intro.pdf",
-  "MongoDB Indexes.pdf",
-  "MongoDB Transactions.pdf",
-    ]
+   MongoDB: [
+   "Connecting to a MongoDB Database.pdf",
+   "Getting Started with MongoDB Atlas.pdf",
+   "Introduction to MongoDB.pdf",
+   "MongoDB Aggregation.pdf",
+   "MongoDB and the Document Model.pdf",
+   "MongoDB Atlas Search.pdf",
+   "MongoDB CRUD Operations Insert and Find Documents.pdf",
+   "MongoDB CRUD Operations Modifying Query Results.pdf",
+   "MongoDB CRUD Operations Replace and Delete Documents.pdf",
+   "MongoDB Data Modeling Intro.pdf",
+   "MongoDB Indexes.pdf",
+   "MongoDB Transactions.pdf",
+  ],
+
+   coursera: [
+   "Administrasi Sistem dan Layanan Infrastruktur TI.pdf",
+   "Dasar-Dasar Dukungan Teknis.pdf",
+   "Keamanan IT Pertahanan terhadap Kejahatan Digital.pdf",
+   "Seluk Beluk Jaringan Komputer.pdf",
+   "Sistem Operasi dan Anda Menjadi Pengguna yang Berdaya.pdf",
+  ],
+
+   dicoding: [
+   "Belajar Dasar Manajemen Proyek.pdf",
+   "Belajar Prinsip Pemrograman SOLID.pdf",
+   "Memulai Pemrograman Dengan C.pdf",
+   "Memulai Pemrograman dengan Haskell.pdf",
+   "Memulai Pemrograman Dengan Java.pdf",
+  ]
 
 };
